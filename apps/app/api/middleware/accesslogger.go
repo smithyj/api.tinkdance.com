55f3e6ea43e2bfad0a16d3ad6044d14655ba1c22
杨圆建 <624508914@qq.com>
2021-10-14 15:38:16 +0800

    fix: AccessLogger 字段增加

--- apps/app/api/middleware/accesslogger.go
+++ apps/app/api/middleware/accesslogger.go
@@ -3,7 +3,10 @@ package middleware
 import (
 	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
+	"net"
+	"time"
 	"tinkdance/apps/app/api/pkg/server"
+	"tinkdance/pkg/trace"
 )
 
 func AccessLogger(srvCtx *server.Context) gin.HandlerFunc {
@@ -12,6 +15,9 @@ func AccessLogger(srvCtx *server.Context) gin.HandlerFunc {
 	}
 
 	return func(c *gin.Context) {
+
+		startAt := time.Now()
+
 		c.Next()
 
 		path := c.Request.URL.Path
@@ -20,17 +26,33 @@ func AccessLogger(srvCtx *server.Context) gin.HandlerFunc {
 			return
 		}
 
+		traceId := ""
+		if v, ok := c.Get(trace.TraceKey); ok {
+			traceId = v.(string)
+		}
+
+		requestId := ""
+		if v, ok := c.Get(trace.RequestKey); ok {
+			requestId = v.(string)
+		}
+
+		host, port, _ := net.SplitHostPort(c.Request.RemoteAddr)
+
 		srvCtx.AccessLogger.WithOptions(zap.WithCaller(false)).With(
+			zap.String("trace_id", traceId),
+			zap.String("request_id", requestId),
+			zap.String("ip", host),
+			zap.String("port", port),
 			zap.String("remote", c.Request.RemoteAddr),
 			zap.String("method", c.Request.Method),
 			zap.String("path", c.Request.URL.Path),
-			zap.String("uri", c.Request.RequestURI),
 			zap.String("host", c.Request.Host),
 			zap.Int("status", c.Writer.Status()),
 			zap.Int("length", c.Writer.Size()),
 			zap.String("proto", c.Request.Proto),
 			zap.String("user-agent", c.Request.UserAgent()),
 			zap.String("referer", c.Request.Referer()),
+			zap.Duration("duration", time.Now().Sub(startAt)),
 		).Info("")
 	}
 }
